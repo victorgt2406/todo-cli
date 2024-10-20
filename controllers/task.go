db79b5252170cfbb1cb23efff41552a3a3cc83ab
victorgt2406 <victorgt2406@github.com>
2024-10-20 20:21:59 +0200

    task controller

---
+++ controllers/task.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"todo-cli/configs"
+	"todo-cli/models"
+
+	"gorm.io/gorm"
+)
+
+type TaskController struct {
+	db *gorm.DB
+}
+
+// TASK Controller CRUD
+
+func NewTaskController() *TaskController {
+	db := configs.InitDB()
+	return &TaskController{db: db}
+}
+
+func (c *TaskController) GetAllTasks() []models.Task {
+	var tasks []models.Task
+	c.db.Find(&tasks)
+	return tasks
+}
+
+func (c *TaskController) GetTask(id int) models.Task {
+	var task models.Task
+	c.db.First(&task, id)
+	return task
+}
+
+func (c *TaskController) CreateTask() int {
+	var task models.Task
+	c.db.Create(&task)
+	return task.ID
+}
+
+func (c *TaskController) UpdateTask(id int, task models.Task) {
+	c.db.Model(&task).Where("id = ?", id).Updates(task)
+}
+
+func (c *TaskController) DeleteTask(id int) {
+	c.db.Delete(&models.Task{}, id)
+}
