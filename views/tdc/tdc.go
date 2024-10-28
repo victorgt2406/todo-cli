0950c3d4c0986842de7423bae863715fa8a1a928
victorgt2406 <victorgt2406@github.com>
2024-10-28 23:18:06 +0100

    leve with esc

--- views/tdc/tdc.go
+++ views/tdc/tdc.go
@@ -72,7 +72,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
 			switch msg.String() {
-			case "ctrl+c", "q":
+			case "ctrl+c", "q", "esc":
 				return m, tea.Quit
 			case "up", "k":
 				if m.cursor > 0 {
