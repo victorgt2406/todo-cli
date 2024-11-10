2ebb630e471ce4ddd005aadb28e9b829a238b588
victorgt2406 <victorgt2406@github.com>
2024-11-10 11:54:11 +0100

    antrhopic support

--- configs/llm.go
+++ configs/llm.go
@@ -19,13 +19,19 @@ func InitLLM() Llm {
 	switch CONFIG.LLM.Provider {
 	case "ollama":
 		return initOllama()
+	case "anthropic":
+		return initAnthropic()
 	default:
 		panic("Invalid LLM provider: " + CONFIG.LLM.Provider)
 	}
 }
 
 func initOllama() Llm {
-	return Llm{url: CONFIG.LLM.Url, model: CONFIG.LLM.Model, apiKey: CONFIG.LLM.ApiKey}
+	return Llm{url: CONFIG.LLM.Url + "/api/chat", model: CONFIG.LLM.Model, apiKey: CONFIG.LLM.ApiKey}
+}
+
+func initAnthropic() Llm {
+	return Llm{url: CONFIG.LLM.Url + "/messages", model: CONFIG.LLM.Model, apiKey: CONFIG.LLM.ApiKey}
 }
 
 func (llm Llm) Chat(context models.Context, message string) (string, error) {
@@ -33,11 +39,16 @@ func (llm Llm) Chat(context models.Context, message string) (string, error) {
 		Role:    "user",
 		Content: message,
 	}
-	url := llm.url + "/api/chat"
+	url := llm.url
 	input := map[string]interface{}{
 		"model":    llm.model,
 		"messages": append(context.Messages, newMessage),
 		"stream":   false,
+	}
+
+	if CONFIG.LLM.Provider == "anthropic" {
+		input["messages"].([]models.ContextMessage)[0].Role = "user"
+		messagesSystemToUser(input["messages"].([]models.ContextMessage))
 	}
 
 	// Convert input to JSON
@@ -59,26 +70,45 @@ func (llm Llm) Chat(context models.Context, message string) (string, error) {
 			Content string `json:"content"`
 		} `json:"message"`
 	}
-	err = json.Unmarshal(body, &response)
+
+	var anthropicResponse struct {
+		Content []struct {
+			Text string `json:"text"`
+			Type string `json:"type"`
+		} `json:"content"`
+		Role string `json:"role"`
+	}
+	if CONFIG.LLM.Provider == "ollama" {
+		err = json.Unmarshal(body, &response)
+	} else if CONFIG.LLM.Provider == "anthropic" {
+		err = json.Unmarshal(body, &anthropicResponse)
+	}
 	if err != nil {
-		// Log the raw response body for debugging
-		fmt.Printf("Raw response body: %s\n", string(body))
 		return "", fmt.Errorf("failed to unmarshal Ollama response: %w\nRaw response: %s", err, string(body))
 	}
 
 	// Get message
-	responseMessage := response.Message.Content
-
+	var responseMessage string
+	if CONFIG.LLM.Provider == "ollama" {
+		responseMessage = response.Message.Content
+	} else if CONFIG.LLM.Provider == "anthropic" {
+		responseMessage = anthropicResponse.Content[0].Text
+	}
 	return responseMessage, nil
 }
 
 func post(url string, apiKey string, body []byte) ([]byte, error) {
+	if CONFIG.LLM.Provider == "anthropic" {
+		body = append(body[:len(body)-1], []byte(`,"max_tokens":1024}`)...)
+	}
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		panic("error creating request: " + err.Error())
 	}
-
-	req.Header.Set("Authorization", "Bearer "+apiKey)
+	if CONFIG.LLM.Provider == "anthropic" {
+		req.Header.Set("x-api-key", apiKey)
+		req.Header.Set("anthropic-version", "2023-06-01")
+	}
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -98,4 +128,12 @@ func post(url string, apiKey string, body []byte) ([]byte, error) {
 	}
 
 	return respBody, nil
+}
+
+func messagesSystemToUser(messages []models.ContextMessage) {
+	for _, message := range messages {
+		if message.Role == "system" {
+			message.Role = "user"
+		}
+	}
 }
