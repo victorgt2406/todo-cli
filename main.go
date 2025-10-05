8eb34bc41b43fc433c146f515d4eb155bd8d5661
Victor <victorgt2406@github.com>
2025-10-05 20:32:45 +0200

    Remove dotenv loading from main function and update build workflow to disable fail-fast strategy

--- main.go
+++ main.go
@@ -5,16 +5,9 @@ import (
 	"todo-cli/commands"
 	"todo-cli/config/configFile"
 	"todo-cli/db"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
-	}
-
 	db, context := db.InitDb()
 	config := configFile.LoadConfig()
 	command := commands.Init(db, context, config)
